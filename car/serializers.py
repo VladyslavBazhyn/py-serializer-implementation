8d1b1d80c0f1886800819fcd94e98b456a082cfd
Vlad_work <mr.darmstadtium@gmail.com>
2024-05-20 17:09:29 +0200

    pass all tests

--- car/serializers.py
+++ car/serializers.py
@@ -3,11 +3,12 @@ from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(max_value=1914, min_value=1)
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_null=True, allow_blank=True)
+    problem_description = serializers.CharField(allow_null=True, allow_blank=True, required=False)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
