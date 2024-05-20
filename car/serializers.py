01e80892dae3fadad301fb9ba25ccb0d39709d3f
Vlad_work <mr.darmstadtium@gmail.com>
2024-05-20 17:14:04 +0200

    flake8

--- car/serializers.py
+++ car/serializers.py
@@ -6,18 +6,32 @@ class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField(max_value=1914, min_value=1)
+    horse_powers = serializers.IntegerField(
+        max_value=1914, min_value=1
+    )
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_null=True, allow_blank=True, required=False)
+    problem_description = serializers.CharField(
+        allow_null=True, allow_blank=True, required=False
+    )
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
-        instance.model = validated_data.get*("model", instance.model)
-        instance.horse_powers = validated_data.get("horse_powers", instance.horse_powers)
-        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
-        instance.problem_description = validated_data.get("problem_description", instance.problem_description)
+        instance.manufacturer = validated_data.get(
+            "manufacturer", instance.manufacturer
+        )
+        instance.model = validated_data.get*(
+            "model", instance.model
+        )
+        instance.horse_powers = validated_data.get(
+            "horse_powers", instance.horse_powers
+        )
+        instance.is_broken = validated_data.get(
+            "is_broken", instance.is_broken
+        )
+        instance.problem_description = validated_data.get(
+            "problem_description", instance.problem_description
+        )
         instance.save()
         return instance
